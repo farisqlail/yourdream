c6d02848a9860db2c0a4585add3a9d76bcf247a6
farisqlail <faris.riskilail@gmail.com>
2024-03-04 23:27:51 +0700

    feat: hitung barang impian

--- pages/stuff/index.vue
+++ pages/stuff/index.vue
@@ -5,32 +5,62 @@
 
       <!-- Input Price -->
       <div class="form-group">
-        <span class="font-medium">Harga Barang Impian:</span>
+        <span class="font-medium">Harga Barang Impian</span>
         <div class="input-custom">
           Rp
-          <input type="text" v-model="hargaBarang" placeholder="8.000.000" />
+          <input
+            type="text"
+            v-model="formattedStuffPrice"
+            placeholder="8.000.000"
+          />
         </div>
       </div>
 
       <!-- Input Monthly Investment -->
-      <div class="form-group">
-        <span class="font-medium">Investasi Tiap Bulan:</span>
+      <div class="form-group" v-if="formattedStuffPrice !== ''">
+        <span class="font-medium">Investasi Tiap Bulan</span>
         <div class="input-custom">
           Rp
-          <input type="text" v-model="investasiTiapBulan" placeholder="1.000.000" />
+          <input
+            type="text"
+            v-model="formattedPriceNow"
+            placeholder="1.000.000"
+          />
         </div>
       </div>
 
       <!-- Input Current Money -->
-      <div class="form-group">
-        <span class="font-medium">Uang yang Dimiliki Sekarang:</span>
+      <div class="form-group" v-if="formattedPriceNow !== ''">
+        <span class="font-medium">Uang yang Dimiliki Sekarang</span>
         <div class="input-custom">
           Rp
-          <input type="text" v-model="uangSekarang" placeholder="5.000.000" />
+          <input
+            type="text"
+            v-model="formattedCurrentMoney"
+            placeholder="5.000.000"
+          />
         </div>
       </div>
 
-      <div class="btn-group">
+      <!-- Input Target Bulan -->
+      <div class="form-group" v-if="formattedCurrentMoney !== ''">
+        <span class="font-medium">Target Bulan Barang Terbeli</span>
+        <div class="input-custom">
+          <input type="text" v-model="targetBulan" placeholder="5" />
+          Bulan
+        </div>
+      </div>
+
+      <!-- Input Annual Interest Rate -->
+      <div class="form-group" v-if="targetBulan !== ''">
+        <span class="font-medium">Suku Bunga Tahunan (%)</span>
+        <div class="input-custom">
+          <input type="text" v-model="annualInterestRate" placeholder="5" />
+          %
+        </div>
+      </div>
+
+      <div class="btn-group" v-if="annualInterestRate !== ''">
         <span class="font-medium">Yuk, hitung strategimu!</span>
         <button class="btn btn-submit" @click="hitung">Lihat Strategimu</button>
       </div>
@@ -38,15 +68,28 @@
       <!-- Modal -->
       <dialog id="modalResult" class="modal">
         <div class="modal-box">
-          <h3 class="font-bold text-lg">Analisa Hasil Perhitungan Investasi Barang Impian</h3>
+          <h3 class="font-bold text-lg">
+            Analisa Hasil Perhitungan Investasi Barang Impian
+          </h3>
           <div class="modal-content">
-            <p>Total uang setelah target bulan: {{ formatCurrency(totalUang) }}</p>
-            <p>Total uang setelah diinvestasikan: {{ formatCurrency(totalInvestasi) }}</p>
-            <p>Bulan yang diperlukan untuk mencapai target: {{ bulanYangDiperlukan }}</p>
+            <div class="total-bunga">
+              <p>
+                Harga barang incaran kamu
+                <strong>Rp {{ formatCurrency(hargaBarang) }} </strong>, total
+                investasi kamu seharusnya
+                <strong>Rp {{ formatCurrency(totalInvestasi) }}</strong
+                >. Kemungkinan tiap Bulan kamu harus menabung
+                <strong>Rp {{ formatCurrency(monthlyMoney) }}</strong>
+              </p>
+            </div>
           </div>
 
           <div class="modal-action">
-            <button class="btn" @click="closeModal">Oke, saya mengerti</button>
+            <form action="">
+              <button class="btn" @click="closeModal">
+                Oke, saya mengerti
+              </button>
+            </form>
           </div>
         </div>
       </dialog>
@@ -74,8 +117,45 @@ export default defineComponent({
       totalUang: 0,
       totalInvestasi: 0,
       bulanYangDiperlukan: 0,
+      monthlyMoney: 0,
       showResults: false,
+      targetBulan: "",
+      annualInterestRate: "",
     };
+  },
+  computed: {
+    formattedStuffPrice: {
+      get() {
+        return this.formatCurrency(this.hargaBarang);
+      },
+      set(value) {
+        // Menghapus karakter non-digit dari nilai input
+        const parsedValue = value.replace(/\D/g, "");
+        this.hargaBarang = parseInt(parsedValue);
+      },
+    },
+
+    formattedPriceNow: {
+      get() {
+        return this.formatCurrency(this.investasiTiapBulan);
+      },
+      set(value) {
+        // Menghapus karakter non-digit dari nilai input
+        const parsedValue = value.replace(/\D/g, "");
+        this.investasiTiapBulan = parseInt(parsedValue);
+      },
+    },
+
+    formattedCurrentMoney: {
+      get() {
+        return this.formatCurrency(this.uangSekarang);
+      },
+      set(value) {
+        // Menghapus karakter non-digit dari nilai input
+        const parsedValue = value.replace(/\D/g, "");
+        this.uangSekarang = parseInt(parsedValue);
+      },
+    },
   },
   methods: {
     formatCurrency(value) {
@@ -85,21 +165,34 @@ export default defineComponent({
     },
     hitung() {
       // Hitung total uang yang akan Anda miliki setelah target bulan
-      this.totalUang = this.uangSekarang + (this.investasiTiapBulan * targetBulan);
-    
-      // Hitung total uang yang akan Anda miliki setelah target bulan jika diinvestasikan
-      // dengan suku bunga tahunan sebesar 5% (misalnya, Anda bisa mengubah sesuai kebutuhan)
-      const sukuBungaTahunan = 0.05;
-      this.totalInvestasi = this.totalUang * Math.pow((1 + sukuBungaTahunan), (targetBulan / 12));
-    
+      this.totalUang =
+        this.uangSekarang +
+        this.investasiTiapBulan * parseInt(this.targetBulan);
+
+      // Use the dynamic annual interest rate
+      const sukuBungaTahunan = parseFloat(this.annualInterestRate) / 100;
+
+      this.totalInvestasi = Math.round(
+        this.totalUang *
+          Math.pow(1 + sukuBungaTahunan, parseInt(this.targetBulan) / 12)
+      );
+
+      this.monthlyMoney = Math.round(
+        this.investasiTiapBulan *
+          Math.pow(1 + sukuBungaTahunan, parseInt(this.targetBulan) / 12)
+      );
+
       // Hitung jumlah bulan yang diperlukan untuk mencapai target harga barang
       const sisaHargaBarang = this.hargaBarang - this.totalInvestasi;
-      this.bulanYangDiperlukan = Math.ceil(sisaHargaBarang / this.investasiTiapBulan);
+      this.bulanYangDiperlukan = Math.ceil(
+        sisaHargaBarang / this.investasiTiapBulan
+      );
 
       // Tampilkan modal
       this.showResults = true;
       this.openModal();
     },
+
     openModal() {
       let modal = document.getElementById("modalResult");
       modal.showModal();
