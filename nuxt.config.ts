48d56161bf8f91f2bf6e25dbaa1d854df0f58e53
farisqlail <faris.riskilail@gmail.com>
2024-06-23 22:51:30 +0700

    icons and padding

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,15 @@ export default defineNuxtConfig({
     preconnect: false,
   },
 
+  app: {
+    head: {
+      viewport: 'initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, width=device-width, height=device-height, target-densitydpi=device-dpi',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: 'https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-19/403828377_374385221919274_5333529820649623523_n.jpg?_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=vhqf_IZkYRUQ7kNvgH33MAk&edm=AEhyXUkBAAAA&ccb=7-5&oh=00_AYDpPgVQ62ptnmpcD6QREkcHvNq76rp4ZWFNKJgXtdcr7w&oe=667E1A19&_nc_sid=cf751b' }
+      ],
+    }
+  },
+
   pwa: {
     /* PWA options */
     manifest: {
@@ -36,23 +45,23 @@ export default defineNuxtConfig({
       theme_color: "#0AB85F",
       icons: [
         {
-          src: "/icon.png",
+          src: "https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-19/403828377_374385221919274_5333529820649623523_n.jpg?_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=vhqf_IZkYRUQ7kNvgH33MAk&edm=AEhyXUkBAAAA&ccb=7-5&oh=00_AYDpPgVQ62ptnmpcD6QREkcHvNq76rp4ZWFNKJgXtdcr7w&oe=667E1A19&_nc_sid=cf751b",
           sizes: "192x192",
           type: "image/png",
         },
         {
-          src: "/icon.png",
+          src: "https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-19/403828377_374385221919274_5333529820649623523_n.jpg?_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=vhqf_IZkYRUQ7kNvgH33MAk&edm=AEhyXUkBAAAA&ccb=7-5&oh=00_AYDpPgVQ62ptnmpcD6QREkcHvNq76rp4ZWFNKJgXtdcr7w&oe=667E1A19&_nc_sid=cf751b",
           sizes: "512x512",
           type: "image/png",
         },
         {
-          src: "/icon.png",
+          src: "https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-19/403828377_374385221919274_5333529820649623523_n.jpg?_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=vhqf_IZkYRUQ7kNvgH33MAk&edm=AEhyXUkBAAAA&ccb=7-5&oh=00_AYDpPgVQ62ptnmpcD6QREkcHvNq76rp4ZWFNKJgXtdcr7w&oe=667E1A19&_nc_sid=cf751b",
           sizes: "512x512",
           type: "image/png",
           purpose: "any",
         },
         {
-          src: "/icon.png",
+          src: "https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-19/403828377_374385221919274_5333529820649623523_n.jpg?_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=vhqf_IZkYRUQ7kNvgH33MAk&edm=AEhyXUkBAAAA&ccb=7-5&oh=00_AYDpPgVQ62ptnmpcD6QREkcHvNq76rp4ZWFNKJgXtdcr7w&oe=667E1A19&_nc_sid=cf751b",
           sizes: "512x512",
           type: "image/png",
           purpose: "maskable",
