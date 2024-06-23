bdcc2dae0644a0d8a4a23fe58eb9e8869433adb2
farisqlail <faris.riskilail@gmail.com>
2024-06-23 18:31:15 +0700

    test

--- pages/index.vue
+++ pages/index.vue
@@ -48,51 +48,7 @@
           <h5>Kunjungi juga</h5>
         </div>
 
-        <div class="carousel rounded-box">
-          <div class="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
-              alt="Burger"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
-              alt="Burger"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
-              alt="Burger"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
-              alt="Burger"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
-              alt="Burger"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
-              alt="Burger"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
-              alt="Burger"
-            />
-          </div>
-        </div>
-        <!-- <div class="carousel rounded-box mt-3">
+        <div class="carousel rounded-box mt-3">
           <div class="carousel-item">
             <nuxt-link
               to="https://www.instagram.com/p/C3Ct7rHSv-n/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
@@ -174,7 +130,7 @@
               </div>
             </nuxt-link>
           </div>
-        </div> -->
+        </div>
       </div>
 
       <Footer />
