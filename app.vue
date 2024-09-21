f2a555519ce3ee88120c587a89fbbcbbd1237496
farisqlail <faris.riskilail@gmail.com>
2024-09-21 13:17:38 +0700

    add: meta dark mode

--- app.vue
+++ app.vue
@@ -1,12 +1,22 @@
 <template>
   <!-- <div> -->
-    <NuxtPwaManifest/>
-    <NuxtPage />
+  <NuxtPwaManifest />
+  <NuxtPage />
   <!-- </div> -->
 </template>
 
 <script>
 import { defineComponent } from "@vue/composition-api";
+import { useHead, useDark, useToggle } from "@vueuse/core";
+
+useHead({
+  meta: [
+    {
+      name: "theme-color",
+      content: isDark.value ? "#000000" : "#f4f4f4", // Warna untuk mode gelap dan terang
+    },
+  ],
+});
 
 export default defineComponent({
   setup() {},
