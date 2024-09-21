b90378400d834daade34b435706949676fab176c
farisqlail <faris.riskilail@gmail.com>
2024-09-21 13:27:49 +0700

    fix: fixing app

--- app.vue
+++ app.vue
@@ -7,16 +7,6 @@
 
 <script>
 import { defineComponent } from "@vue/composition-api";
-import { useHead, useDark, useToggle } from "@vueuse/core";
-
-useHead({
-  meta: [
-    {
-      name: "theme-color",
-      content: isDark.value ? "#000000" : "#f4f4f4", // Warna untuk mode gelap dan terang
-    },
-  ],
-});
 
 export default defineComponent({
   setup() {},
